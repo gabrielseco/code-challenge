f28f49375f1f378f51ad6ff667f765333c8778cc
gabriel <ggarciaseco@gmail.com>
2017-07-16 09:37:00 +0200

    Add history prop to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 // @flow
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './Header.css';
 
-const Header = (props: { title: string }) => (
-  <header className="header fixed">
+const Header = (props: { title: string, history: any }) => (
+  <header className="header fixed" onClick={() => props.history.push('/')}>
     <h2 className="title">{props.title}</h2>
   </header>
 );
@@ -12,4 +13,5 @@ Header.defaultProps = {
   title: 'Billin',
 };
 
-export default Header;
+
+export default withRouter(Header);
