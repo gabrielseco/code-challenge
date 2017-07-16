afc204b1d7deaa5ec5ca919ec2a48db44f740138
gabriel <ggarciaseco@gmail.com>
2017-07-16 09:39:52 +0200

    Add support with link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 // @flow
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import './Header.css';
 
 const Header = (props: { title: string, history: any }) => (
   <header className="header fixed" onClick={() => props.history.push('/')}>
-    <h2 className="title">{props.title}</h2>
+    <Link to="/">
+      <h2 className="title">{props.title}</h2>
+    </Link>
   </header>
 );
 
